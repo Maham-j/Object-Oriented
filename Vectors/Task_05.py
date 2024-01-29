d4715cb4ed7953977b3f6a35106d4c023a6b2e49
Maham.J <139694482+Maham-j@users.noreply.github.com>
2024-01-30 01:36:49 +0500

    Create Task_05.py

---
+++ Vectors/Task_05.py
@@ -0,0 +1,27 @@
+#task_05
+class Vector:
+    pass
+
+
+def equality(v1,v2):
+    if v1.x == v2.x and v1.y == v2.y and v1.z == v2.z :
+        return 'Vectors are equal'
+    else:
+        return 'Vectors are not equal'
+    
+def vectors():
+    v = Vector()
+    v.x = 3
+    v.y = 5
+    v.z = 2
+    
+    m = Vector()
+    m.x = 6
+    m.y = 8
+    m.z = 1
+     
+
+    result = equality(v,m)
+    print(result)
+    
+vectors()
